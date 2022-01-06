c7b1bb1de9a74c63493b9735d16d206c4edb43c5
Alex Kokobane <alexkokobane@gmail.com>
2022-01-06 20:06:21 +0200

    removed old shop model

--- src/models/shop-model.ts
+++ src/models/shop-model.ts
@@ -7,52 +7,8 @@ const ShopSchema = new mongoose.Schema({
 		unique: true
 	},
 	pricePlan: String,
-	warrantyAll: {
-		warranty: String,
-		evaldates: [String]
-	},
-	warrantyIndividual: [{
-		productId: String,
-		warranty: String,
-		evaldates: [String]
-	}],
 	scope: [String],
 	email: String,
-	customers: [{
-		firstName: String,
-		displayName: String,
-		email: String,
-		allowsMarketing: Boolean,
-		ratedProducts: [{
-			productId: String,
-			productName: String,
-			avgRating: Number,
-			orderDate: {
-				type: Date,
-				default: Date.now()
-			},
-			reviewDates: [String],
-			comments: [{
-				rating: Number,
-				body: String,
-				views: Number,
-				replies: [String],
-				createdAt: {
-					type: Date,
-					default: Date.now()
-				}
-			}]
-		}]
-	}],
-	discountedProduct: {
-		productId: String,
-		defaultPrice: String,
-		discountPercent: Number,
-		discountedPrice: String
-	},
-	analytics: [{
-		productId: String
-	}],
 	emailTemplate: String,
 	createdAt: {
 		type: Date,
