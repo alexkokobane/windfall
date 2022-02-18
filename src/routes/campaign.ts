a16473705936a7ff9527ab4bfdd7a41eb8b8b80a
Alex Kokobane <alexkokobane@gmail.com>
2022-02-18 13:15:40 +0200

    usage limiter added to template

--- src/routes/campaign.ts
+++ src/routes/campaign.ts
@@ -800,7 +800,20 @@ campaign.post('/store', checkApiAuth, async (req, res) => {
 			return res.status(404).send("Did not save, giveaway does not exist")
 		}
 		// check for the quota
-		templateGate(req, res, session.shop)
+		//templateGate(req, res, session.shop)
+		const templates: any = await Saved.find({'shop': session.shop})
+		const shopper: any = await Shop.findOne({'shop': session.shop})
+		let count = templates.length
+		let plan = shopper.pricePlan
+		console.log("It runs and the count is "+count+" on a "+plan+" plan")
+		if(plan === "Starter" && count > 2) {
+			console.log("It passes here")
+			return res.status(403).send("Sorry, you have reached your quota")
+		} else if(plan === "Standard" && count > 6){
+			return res.status(403).send("Sorry, you have reached your quota")
+		} else if(plan ==="Ultimate" && count > 1000){
+			return res.status(403).send("Sorry, you have reached your quota")
+		}
 		// check weather it already has a template from which it was made.
 		if(giveaway.templateId){
 			const checker = await Saved.findOne({
