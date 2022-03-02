b0ec58f399fbeb90e046a26867ea41b84cc38515
Alex Kokobane <alexkokobane@gmail.com>
2022-03-02 21:01:58 +0200

    grand choose winner ui finesse

--- src/public/main.js
+++ src/public/main.js
@@ -3554,7 +3554,7 @@ $(document).ready(function(e){
 							$("#GchooseWinner").click(function(){
 								$(this).addClass("Polaris-Button--loading")
 								$(`#GchooseWinnertext`).before(`
-									<span class="Polaris-Button__Spinner">
+									<span id="GchooseWinnerSpinner" class="Polaris-Button__Spinner">
 										<span class="Polaris-Spinner Polaris-Spinner--sizeSmall">
 											<svg viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
             									<path d="M7.229 1.173a9.25 9.25 0 1011.655 11.412 1.25 1.25 0 10-2.4-.698 6.75 6.75 0 11-8.506-8.329 1.25 1.25 0 10-.75-2.385z"></path>
@@ -3565,6 +3565,26 @@ $(document).ready(function(e){
           								</span>
           							</span>
           						`)
+
+          						$.ajax({
+          							url: `/campaign/grand/${data.id}/choose-winners`,
+          							type: "POST",
+          							contentType: "application/json",
+          							success: function(data){
+          								alert(data)
+          								location.reload()
+          							},
+          							error: function(data){
+          								if(data.responseText === "Unauthorized"){
+											return location.href="/"
+										} else if(data.responseText === "Forbidden"){
+											return location.href="/billing/plans"
+										}
+										$("#GchooseWinner").removeClass("Polaris-Button--loading")
+										$("#GchooseWinnerSpinner").remove()
+										alert(data.responseText)
+          							}
+          						})
 							})
 						}
 					},
