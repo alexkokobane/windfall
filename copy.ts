6fb7dda550d321091c62e00abd5ae152812fe472
Alex Kokobane <alexkokobane@gmail.com>
2022-03-16 13:58:39 +0200

    fixed the altjs script build process

--- copy.ts
+++ copy.ts
@@ -3,7 +3,7 @@ import shell from 'shelljs'
 const buildFolder = './dist/'
 
 const files = new Set(['package.json', 'package-lock.json'])
-const folders = new Set(['./src/views', './src/public'])
+const folders = new Set(['./src/views', './src/public', './src/files'])
 
 // Copy Folders
 folders.forEach((folder) => {
