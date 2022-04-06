5ef76b87420cec07cef6a4b647b401b5ac031e47
Alex Kokobane <alexkokobane@gmail.com>
2022-04-06 16:39:29 +0200

    created the quota watching middlewares

--- src/utils/middlewares/quota.ts
+++ src/utils/middlewares/quota.ts
@@ -7,3 +7,40 @@ import { templateGate } from '../quotas'
 import { forCommon, forStarter, forStandard, forUltimate } from './price-plan'
 import { divide, renderFor } from '../render-divider'
 import { generateDiscountCode } from '../functions'
+
+const quota = async (req: Request, res: Response, next: NextFunction) => {
+	try {
+		const session = await Shopify.Utils.loadCurrentSession(req, res, true)
+		const quota = await Quota.findOne({'shop': session.shop})
+		const month = new Date(Date.now()).toISOString().substring(0, 7)
+		const maxQuota = quota.entries[quota.entries.length - 1].maxValue
+		const valueQuota = quota.entries[quota.entries.length - 1].value
+		if(valueQuota > maxQuota){
+			return res.redirect("/billing/change")
+		}
+
+		return next()
+	} catch(err: any){
+		console.log(err)
+		return err
+	}
+}
+
+const quotaApi = async (req: Request, res: Response, next: NextFunction) => {
+	try {
+		const session = await Shopify.Utils.loadCurrentSession(req, res, true)
+		const quota = await Quota.findOne({'shop': session.shop})
+		const maxQuota = quota.entries[quota.entries.length - 1].maxValue
+		const valueQuota = quota.entries[quota.entries.length - 1].value
+		if(valueQuota > maxQuota){
+			return res.status(403).send("You have reached your usage limit, cannot create resource. Upgrade to continue.")
+		}
+
+		return next()
+	} catch(err: any){
+		console.log(err)
+		return err
+	}
+}
+
+export { quota, quotaApi }
