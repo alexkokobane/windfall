ba4c7cd31e42691340f6ea2accd7e193fda1afa7
Alex Kokobane <alexkokobane@gmail.com>
2022-05-04 19:44:05 +0200

    only installed stores can login

--- src/api/login.ts
+++ src/api/login.ts
@@ -21,14 +21,15 @@ login.post('/', loggedInCtx, async (req, res) => {
 		const holder: string[] = url.split(".")
 		console.log(holder)
 		if(holder[1] !== "myshopify"){
-			return res.status(403).send("Please use your 'myshopify' url.")
+			return res.status(401).send("Please use your 'myshopify' url.")
 		} else if(holder[2] !== "com"){
-			return res.status(403).send("Error! The top level domain of every 'myshopify' url is dot com.")
+			return res.status(401).send("Error! The top level domain of every 'myshopify' url is dot com.")
 		}
 
-		res.status(200).send("/auth?shop="+url)
+		return res.status(401).send("Non-existent shop detected! Install Windfall from the Shopify App Store.")
 	} catch(err: any) {
-		console.log(err)
+		//console.log(err)
+		return res.status(401).send("Error: "+err)
 	}
 })
 
